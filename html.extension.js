9719cf3df935e5e20c86f15a66d364323b1e9652
JingAn Chen <lzsoft.cja@gmail.com>
2016-11-23 17:06:04 -0500

    Use Bower again.

--- html.extension.js
+++ html.extension.js
@@ -1,2 +1,58 @@
-let Math = require('./window/Math.js');
-let NodeList = require('./window/NodeList.js');
+'use strict';
+window.Math.randomRange = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+window.Number.prototype.toRad = function () {
+    return this * Math.PI / 180;
+};
+window.Math.calculateDistance = function (lat1, lon1, lat2, lon2) {
+    var R = 6371, // km
+        dLat = (lat2 - lat1).toRad(),
+        dLon = (lon2 - lon1).toRad(),
+        a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * Math.sin(dLon / 2) * Math.sin(dLon / 2),
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)),
+        d = R * c;
+    return d;
+};
+window.NodeList.prototype.addEventListener = function (eventName, handler) {
+    for (var i = 0; i < this.length; i++) {
+        this[i].addEventListener(eventName, handler);
+    }
+    return this;
+};
+window.NodeList.prototype.remove = function () {
+    for (var i = 0; i < this.length; i++) {
+        this[i].remove();
+    }
+    return this;
+};
+window.NodeList.prototype.exec = function (callback) {
+    for (var i = 0; i < this.length; i++) {
+        callback.call(this[i]);
+    }
+    return this;
+};
+window.NodeList.prototype.setAttribute = function (key, value) {
+    for (var i = 0; i < this.length; i++) {
+        this[i].setAttribute(key, value);
+    }
+    return this;
+};
+window.NodeList.prototype.removeAttribute = function (key) {
+    for (var i = 0; i < this.length; i++) {
+        this[i].removeAttribute(key);
+    }
+    return this;
+};
+window.NodeList.prototype.addClass = function (name) {
+    for (var i = 0; i < this.length; i++) {
+        this[i].classList.add(name);
+    }
+    return this;
+};
+window.NodeList.prototype.removeClass = function (name) {
+    for (var i = 0; i < this.length; i++) {
+        this[i].classList.remove(name);
+    }
+    return this;
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+# window.Math
+
+```javascript
+window.Math.randomRange(min, max);
+window.Number.prototype.toRad();
+window.Math.calculateDistance(lat1, lon1, lat2, lon2);
+```
+
+# window.NodeList
+
+```javascript
+window.NodeList.prototype.addEventListener(eventName, handler);
+window.NodeList.prototype.remove();
+window.NodeList.prototype.exec(callback);
+window.NodeList.prototype.setAttribute(key, value);
+window.NodeList.prototype.removeAttribute(key);
+window.NodeList.prototype.addClass(name);
+window.NodeList.prototype.removeClass(name);
+```
