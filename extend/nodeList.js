4a386bb94c2ff017feaf38d75b90cf232b2c16dd
JingAn Chen <lzsoft.cja@gmail.com>
2017-01-19 11:23:53 -0500

    Added a dispatchEvent() to nodeList.

--- extend/nodeList.js
+++ extend/nodeList.js
@@ -1,41 +1,47 @@
 window.NodeList.prototype.addEventListener = function (eventName, handler) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].addEventListener(eventName, handler);
     }
     return this;
 };
+window.NodeList.prototype.dispatchEvent = function (eventName) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].dispatchEvent(new Event(eventName));
+    }
+    return this;
+};
 window.NodeList.prototype.remove = function () {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].remove();
     }
     return this;
 };
 window.NodeList.prototype.exec = function (callback) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         callback(this[i]);
     }
     return this;
 };
 window.NodeList.prototype.setAttribute = function (key, value) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].setAttribute(key, value);
     }
     return this;
 };
 window.NodeList.prototype.removeAttribute = function (key) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].removeAttribute(key);
     }
     return this;
 };
 window.NodeList.prototype.addClass = function (name) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].classList.add(name);
     }
     return this;
 };
 window.NodeList.prototype.removeClass = function (name) {
-    for (var i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         this[i].classList.remove(name);
     }
     return this;
